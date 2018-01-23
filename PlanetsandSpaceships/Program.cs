b81641930c07091d4df5794d38fc125829609a97
waamber <amberastuart@aol.com>
2018-01-22 19:00:49 -0600

    looping through space dictionary

--- PlanetsandSpaceships/Program.cs
+++ PlanetsandSpaceships/Program.cs
@@ -21,12 +21,29 @@ namespace PlanetsandSpaceships
             planetList.Insert(1, "Venus");
             planetList.Insert(2, "Earth");
             planetList.Add("Pluto");
+            planetList.Remove("Pluto");
 
             List<string> rockyPlanets = planetList.GetRange(0, 4);
-
-            foreach (var planet in rockyPlanets)
+            var spacecraft = new Dictionary<string, List<string>>
             {
-                Console.WriteLine(planet);
+                {"Mariner", new List<string> { "Venus", "Mars", "Mercury"} },
+                {"Messenger", new List<string> { "Mercury"} },
+                {"Voyager 1", new List<string> { "Saturn"} },
+                {"Viking", new List<string> {"Mars" } },
+                {"Voyager 2", new List<string> {"Uranus", "Neptune" } },
+                {"Pioneer 10", new List<string> {"Jupiter" } },
+                {"Galileo", new List<string> {"Earth"} }
+            };
+
+            foreach (var planet in planetList)
+            {
+                foreach (var craft in spacecraft)
+                {
+                    if (craft.Value.Contains(planet))
+                    {
+                        Console.WriteLine($"{craft.Key} visited: {planet}");
+                    }
+                }
             }
 
             Console.ReadKey();
